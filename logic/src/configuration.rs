97a8e54942a49bb841a3c89a0db24dec5659be4b
Manuel Woelker <manuel.woelker+jj@gmail.com>
2025-08-10 22:38:45 +0200

    feat(logic) basic string templating in config

--- logic/src/configuration.rs
+++ logic/src/configuration.rs
@@ -1,13 +1,45 @@
 use kdl::{KdlDocument, KdlNode};
 use miette::{GraphicalReportHandler, GraphicalTheme};
-use std::collections::HashMap;
-use tool_tool_base::result::{Context, ToolToolResult, bail};
+use std::collections::BTreeMap;
+use std::str::FromStr;
+use tool_tool_base::result::{Context, ToolToolError, ToolToolResult, bail};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd)]
+pub enum DownloadPlatform {
+    Default,
+    Linux,
+    MacOS,
+    Windows,
+}
+impl DownloadPlatform {
+    pub fn to_str(&self) -> &'static str {
+        match self {
+            DownloadPlatform::Default => "default",
+            DownloadPlatform::Windows => "windows",
+            DownloadPlatform::Linux => "linux",
+            DownloadPlatform::MacOS => "macos",
+        }
+    }
+}
+
+impl FromStr for DownloadPlatform {
+    type Err = ToolToolError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "default" => Ok(DownloadPlatform::Default),
+            "windows" => Ok(DownloadPlatform::Windows),
+            "linux" => Ok(DownloadPlatform::Linux),
+            "macos" => Ok(DownloadPlatform::MacOS),
+            other => bail!("Unknown download platform: '{other}'"),
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct ToolConfiguration {
     pub name: String,
     pub version: String,
-    pub download_urls: HashMap<String, String>,
+    pub download_urls: BTreeMap<DownloadPlatform, String>,
 }
 
 #[derive(Debug)]
@@ -45,7 +77,7 @@ pub fn parse_configuration_from_kdl(
                         .value()
                         .as_string()
                         .expect("Expected tool version to be a string");
-                    let mut download_urls = HashMap::new();
+                    let mut download_urls = BTreeMap::new();
                     for tool_child in children(tool_node) {
                         match tool_child.name().value() {
                             "download" => {
@@ -57,7 +89,8 @@ pub fn parse_configuration_from_kdl(
                                         .value()
                                         .as_string()
                                         .expect("Expected download url to be a string");
-                                    download_urls.insert(os, url.to_string());
+                                    download_urls
+                                        .insert(DownloadPlatform::from_str(&os)?, url.to_string());
                                 }
                             }
                             other => bail!("Unknown tool child: '{other}'"),
@@ -82,9 +115,23 @@ fn children<'a>(node: &'a KdlNode) -> impl IntoIterator<Item = &'a KdlNode> + 'a
     node.children().map(|doc| doc.nodes()).into_iter().flatten()
 }
 
+pub fn expand_configuration_template_expressions(
+    configuration: &mut ToolToolConfiguration,
+) -> ToolToolResult<()> {
+    for tool in &mut configuration.tools {
+        for url in tool.download_urls.values_mut() {
+            let new_url = url.replace("${version}", &tool.version);
+            *url = new_url;
+        }
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::configuration::parse_configuration_from_kdl;
+    use crate::configuration::{
+        expand_configuration_template_expressions, parse_configuration_from_kdl,
+    };
     use expect_test::{Expect, expect};
     use tool_tool_base::result::ToolToolResult;
 
@@ -157,8 +204,8 @@ mod tests {
                         name: "lsd",
                         version: "0.17.0",
                         download_urls: {
-                            "windows": "https://github.com/Peltoche/lsd/releases/download/0.17.0/lsd-0.17.0-x86_64-pc-windows-msvc.zip",
-                            "linux": "https://github.com/Peltoche/lsd/releases/download/0.17.0/lsd-0.17.0-x86_64-unknown-linux-gnu.tar.gz",
+                            Linux: "https://github.com/Peltoche/lsd/releases/download/0.17.0/lsd-0.17.0-x86_64-unknown-linux-gnu.tar.gz",
+                            Windows: "https://github.com/Peltoche/lsd/releases/download/0.17.0/lsd-0.17.0-x86_64-pc-windows-msvc.zip",
                         },
                     },
                 ],
@@ -199,6 +246,47 @@ mod tests {
                ·         [35;1m╰── [35;1mnot node name[0m[0m
                ╰────
             [36m  help: [0mThis can be any string type, including a quoted, raw, or multiline string, as well as a plain identifier string.
+        "#]]
+    );
+
+    fn test_parse_and_expand(kdl: &str, expected: Expect) -> ToolToolResult<()> {
+        let mut config = parse_configuration_from_kdl(".tool-tool.v2.kdl", kdl)?;
+        expand_configuration_template_expressions(&mut config)?;
+        expected.assert_debug_eq(&config);
+        Ok(())
+    }
+
+    macro_rules! test_parse_and_expand(
+        ($name:ident, $kdl:expr, $expected:expr) => {
+            #[test]
+            fn $name() -> ToolToolResult<()> {
+                test_parse_and_expand($kdl, $expected)
+            }
+            });
+
+    test_parse_and_expand!(
+        test_expand_version,
+        r#"tools {
+                lsd "0.17.0" {
+                    download {
+                        linux "https://github.com/Peltoche/lsd/releases/download/${version}/lsd-${version}-x86_64-unknown-linux-gnu.tar.gz"
+                        windows "https://github.com/Peltoche/lsd/releases/download/${version}/lsd-${version}-x86_64-pc-windows-msvc.zip"
+                    }
+                }
+            }"#,
+        expect![[r#"
+            ToolToolConfiguration {
+                tools: [
+                    ToolConfiguration {
+                        name: "lsd",
+                        version: "0.17.0",
+                        download_urls: {
+                            Linux: "https://github.com/Peltoche/lsd/releases/download/0.17.0/lsd-0.17.0-x86_64-unknown-linux-gnu.tar.gz",
+                            Windows: "https://github.com/Peltoche/lsd/releases/download/0.17.0/lsd-0.17.0-x86_64-pc-windows-msvc.zip",
+                        },
+                    },
+                ],
+            }
         "#]]
     );
 }
