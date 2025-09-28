3637417bf33f4e7d42c73d3e6e0c90aa43e53ca6
Manuel Woelker <github@manuel.woelker.org>
2025-09-28 15:36:49 +0200

    chore(ci): add release workflow

---
+++ scripts/create_release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+LEVEL=${1:-minor}
+
+ROOT_DIR=$(dirname $0)/..
+cd $ROOT_DIR
+
+#cargo release -v --workspace --tag-prefix v --all-features $LEVEL
+#cargo publish --workspace --all-features
+cargo set-version --bump minor
+VERSION=$(cargo pkgid --manifest-path cli/Cargo.toml | cut -d "@" -f2)
+git commit -a -m "chore(release): Release v$VERSION"
+echo VERSION: $VERSION
+git tag -a v$VERSION -m "Release v$VERSION"
+git push origin v$VERSION
+cargo publish --workspace --all-features
